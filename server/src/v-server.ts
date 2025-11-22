8d2f2aaf88773fcec6f2db88cc880a8435cc78f9
Md. Jahid Hasan <itsjahidhasan@outlook.com>
2025-11-23 00:43:07 +0600

    vercel deployment ready

--- server/src/v-server.ts
+++ server/src/v-server.ts
@@ -11,6 +11,4 @@ const PORT = process.env.PORT || 5000;
 
 console.log("Starting server...");
 
-app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+export default app;
